11c8d6dfc5b6ddf1dbd58555a17c9dff7db04578
vandana24 <vandana.inotech@aesl.in>
2023-07-25 18:23:43 +0530

    Added add card functionality and showing sucesseful added message

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let productHtml = ''
-products.forEach((product)=>{
+products.forEach((product) => {
     productHtml += `
 
     <div class="card-wrap">
@@ -13,44 +13,58 @@ products.forEach((product)=>{
         $${(product.priceCents / 100).toFixed(2)}
     </div>
     <div class="product-count">
-        <select data-attr="dropdown" id="input">
+        <select data-attr="dropdown" id="input" class="js-quantity-selector-${product.id}">
             <option value="1" id="1">1</option>
             <option value="2" id="2">2</option>
-            <option value="1" id="1">3</option>
-            <option value="2" id="2">4</option>
-            <option value="1" id="1">5</option>
-            <option value="2" id="2">6</option>
-            <option value="1" id="1">7</option>
-            <option value="2" id="2">8</option>
-            <option value="1" id="1">9</option>
-            <option value="2" id="2">10</option>
+            <option value="3" id="3">3</option>
+            <option value="4" id="4">4</option>
+            <option value="5" id="5">5</option>
+            <option value="6" id="6">6</option>
+            <option value="7" id="7">7</option>
+            <option value="8" id="8">8</option>
+            <option value="9" id="9">9</option>
+            <option value="10" id="10">10</option>
         </select>
     </div>
-    <p class="sucessesfully-added js-added-succesfully"><span class="cheqmark">✓</span><span>Add successfully</span></p>
+    <div class="sucessesfully-added js-added-succesfully-${product.id}"><div class="align-checkmark"><img src="images/icons/checkmark.png">Added sucessesfully</div></div>
     <button class="add-cart-btn js-add-btn" data-product-name="${product.id}">Add to cart</button>
 </div>
     `
 })
 document.querySelector('.body-wrap').innerHTML = productHtml;
 
-function sucessesfullyAdded(){
+function sucessesfullyAdded() {
 }
-document.querySelectorAll('.js-add-btn').forEach ((button, index) => {
-    button.addEventListener('click', ()=>{ 
-        const productId = button.dataset.id;
+document.querySelectorAll('.js-add-btn').forEach((button, index) => {
+    button.addEventListener('click', (e) => {
+
+        const productId = button.dataset.productName;
+        
+        // Added sucessefully msg
+
+        const added = document.querySelector(`.js-added-succesfully-${productId}`);
+        added.classList.add('active');
+        setTimeout(()=>{
+            added.classList.remove('active');
+        },1000);
         let matchingItem;
         cart.forEach((item) => {
-            if(productId == item.productId){
+            if (productId == item.productId) {
                 matchingItem = item;
             }
         })
-        if(matchingItem) {
-            matchingItem.quantity += 1;
+        const quantitySelector = document.querySelector(
+            `.js-quantity-selector-${productId}`
+        );
+        // quantitySelector.value
+        const quantity = Number(quantitySelector.value);
+        if (matchingItem) {
+            matchingItem.quantity += quantity;
         } else {
-        cart.push({productId: productId, quantity: 1})
+            cart.push({ productId: productId, quantity: quantity })
         }
         let cartQuantity = 0;
-        cart.forEach( (item) => {
+        cart.forEach((item) => {
             cartQuantity += item.quantity;
         })
         document.querySelector('.js-cart-count').innerHTML = cartQuantity;
